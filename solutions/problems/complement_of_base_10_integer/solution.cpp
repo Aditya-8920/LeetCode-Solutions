2b35bcef7b7c7c67149a0e5712eb6465c2f9bddf
Aditya-8920 <adityasrivastava8920@gmail.com>
2025-11-23 09:50:12 +0000

    Sync LeetCode submission - Complement of Base 10 Integer (cpp)

--- solutions/problems/complement_of_base_10_integer/solution.cpp
+++ solutions/problems/complement_of_base_10_integer/solution.cpp
@@ -6,11 +6,11 @@ public:
 
         if (n==0){
             return 1;
+
         }
-        while (m!=0){
+        while(m!=0){
             mask= (mask<<1) | 1;
             m= m>>1;
-           
         }
         int ans= (~n) & mask;
         return ans;
