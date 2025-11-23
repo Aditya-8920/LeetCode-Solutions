65d2230fdf4adc2f9dcf3b0bda792cd14564aa8d
Aditya-8920 <adityasrivastava8920@gmail.com>
2025-11-23 09:37:04 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- solutions/problems/reverse_integer/solution.cpp
+++ solutions/problems/reverse_integer/solution.cpp
@@ -5,11 +5,11 @@ public:
         while (x!=0){
             int digit= x%10;
 
-            if ((ans<INT_MIN/10) || (ans> INT_MAX/10)){
+            if ((ans> INT_MAX/10) | (ans< INT_MIN/10)){
                 return 0;
             }
-            ans= (ans*10)+ digit;
-            x=x/10;
+            ans= (ans*10) + digit;
+            x= x/10;
         }
         return ans;
     }
