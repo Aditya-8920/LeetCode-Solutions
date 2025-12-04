c4fb88f2cdf5b72bb84c1d7eca88ec7bb33f9c2d
Aditya-8920 <adityasrivastava8920@gmail.com>
2025-12-04 10:48:41 +0000

    Sync LeetCode submission - Unique Number of Occurrences (cpp)

---
+++ solutions/problems/unique_number_of_occurrences/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector <int> v;
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<arr.size(); i++){
+            int count=1;
+            while(i+1<arr.size() && arr[i]==arr[i+1]){
+                count++;
+                i++;
+            }
+            v.push_back(count);
+        }
+        sort(v.begin(), v.end());
+        for(int i=0; i<v.size()-1; i++){
+            if(v[i]==v[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
