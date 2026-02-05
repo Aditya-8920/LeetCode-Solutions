fb687ffa5a3baaefaa15a39731d90b2b59e5fbb2
Aditya-8920 <adityasrivastava8920@gmail.com>
2026-02-05 11:03:08 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

---
+++ solutions/problems/spiral_matrix/solution.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        vector<int> ans;
+        int row= matrix.size(); //no. of rows
+        int col= matrix[0].size();//no of col
+
+        int count= 0;
+        int total= row*col;
+
+        //ye saare neeche indexes hain(index initialisation)
+        int startingRow= 0;
+        int startingCol= 0;
+        int endingRow= row-1;
+        int endingCol= col-1;
+
+        while(count< total){
+            //print starting row
+            for(int index= startingCol; count< total && index<= endingCol; index++){
+                ans.push_back(matrix[startingRow][index]);
+                count++;//jab bhi koi element add karenge toh count ko badhayenge
+            }
+            startingRow++;
+
+            //print ending column
+            for(int index= startingRow; count< total && index<= endingRow; index++){
+                ans.push_back(matrix[index][endingCol]);//index row ko darsha raha hai isliye vo pahle aayega 
+                count++;
+            }
+            endingCol--;
+
+            //print ending row
+            for(int index= endingCol; count< total && index>= startingCol; index--){
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+
+            //print starting column
+            for(int index= endingRow; count< total && index>= startingRow; index--){
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+
+};
