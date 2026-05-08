18260f5a7d1b13cf3402ad01151a0baee0ffa090
Aditya-8920 <adityasrivastava8920@gmail.com>
2026-05-08 06:50:44 +0000

    Sync LeetCode submission - Type of Triangle (cpp)

---
+++ solutions/problems/type_of_triangle/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
+        if(nums[0]+ nums[1]<= nums[2]){
+            return "none";
+        }
+        if(s.size()== 1){
+            return "equilateral";
+        }
+        else if(s.size()== 2){
+            return "isosceles";
+        }
+        else{
+            return "scalene";
+        }
+    }
+};
