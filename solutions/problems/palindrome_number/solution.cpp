b68a694eebb2d8f180f3a37da992a9c44970fd3c
Aditya-8920 <adityasrivastava8920@gmail.com>
2026-05-13 12:10:03 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- solutions/problems/palindrome_number/solution.cpp
+++ solutions/problems/palindrome_number/solution.cpp
@@ -4,9 +4,8 @@ public:
         string num= to_string(x);
         int s= 0;
         int e= num.length()-1;
-
         while(s<= e){
-            if(num[s]!= num[e]){
+            if(num[s] != num[e]){
                 return false;
             }
             s++;
