44d60cebbc1fd8bc58988870a6cdca3262479874
Aditya-8920 <adityasrivastava8920@gmail.com>
2026-05-17 05:48:14 +0000

    Sync LeetCode submission - Middle of the Linked List (cpp)

--- solutions/problems/middle_of_the_linked_list/solution.cpp
+++ solutions/problems/middle_of_the_linked_list/solution.cpp
@@ -11,16 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if(head == NULL ||  head->next == NULL){
+        if(head == NULL || head->next == NULL){
             return head;
         }
         ListNode* slow= head;
-        ListNode* fast= head->next;
-        while(fast != NULL){
-            fast= fast->next;
-            if(fast!= NULL){
-                fast= fast->next;
-            }
+        ListNode* fast= head;
+
+        while(fast != NULL && fast->next != NULL){
+            fast= fast->next->next;
             slow= slow->next;
         }
         return slow;
